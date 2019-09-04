d820f05ed27ef39471814d164200eb2e6465a7ad
soulboy <rtsfan1024@gmail.com>
2019-09-04 14:24:34 +0800

    test_version_1

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+    private int xdclass;
+    private static final int TMF = 1;
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+        System.out.println("hello springboot!");
+    }
+
+    public int getXdclass() {
+        return xdclass;
+    }
+
+    public void setXdclass(int xdclass) {
+        this.xdclass = xdclass;
+    }
+}
