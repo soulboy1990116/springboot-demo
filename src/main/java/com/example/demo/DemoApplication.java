3ac6c9532094f7c945079b8d9d8f54075c2b8045
soulboy <rtsfan1024@gmail.com>
2019-09-05 12:22:31 +0800

    v2:修改字符串

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
     private int xdclass;
     private static final int TMF = 3;
-    private String version = v2;
+    private String version = "v2";
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
