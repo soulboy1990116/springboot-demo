aa0b0897459aa68338f07affc16414296d724663
soulboy <rtsfan1024@gmail.com>
2019-09-05 12:21:03 +0800

    v1

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
     private int xdclass;
     private static final int TMF = 3;
+    private String version = "v1";
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
